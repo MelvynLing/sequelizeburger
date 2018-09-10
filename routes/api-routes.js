c60708a6e930d64513671120eb9d96a34e99f1f6
Mel Ling <melvyn.ling@gmail.com>
2018-09-09 23:18:41 -0400

    removed comments, looking into why img wont load. looking into heroku issues

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,29 +1,18 @@
-// *********************************************************************************
-// api-routes.js - this file offers a set of routes for displaying and saving data to the db
-// *********************************************************************************
 
-// Dependencies
-// =============================================================
-
-// Requiring our models
 var db = require("../models");
 
-// Routes
-// =============================================================
 module.exports = app => {
-  // on page load or on redirects
+
   app.get("/", (req, res) => {
     res.redirect("/burgers");
   });
 
-  // on page load or on redirects
   app.get("/burgers", (req, res) => {
     var query = {};
     if (req.query.CustomerId) {
       query.Customer = req.query.CustomerId;
     }
 
-    // if the CustomerId field of the burgers table is not empty, include the customer of that id in the results
     db.Burger
       .findAll({
         include: db.Customer,
@@ -35,7 +24,6 @@ module.exports = app => {
       });
   });
 
-  // when submit button is pressed
   app.post("/burgers/create", (req, res) => {
     db.Burger
       .create({
@@ -46,7 +34,6 @@ module.exports = app => {
       });
   });
 
-  // when devour button is pressed
   app.put("/burgers/update", (req, res) => {
     var customerName = req.body.eaten_by;
     // var customerNameTrim = customerName.toLowerCase().trim();
@@ -57,11 +44,9 @@ module.exports = app => {
       })
       .then(data => {
         if (data.length > 0) {
-          // if customer already exists in database, devour burger
           console.log("customer already exists");
           devour(data[0].dataValues.id);
         } else {
-          // if customer does not exist in database, create new customer, then devour burger
           console.log("creating new customer");
           db.Customer
             .create({
@@ -73,8 +58,6 @@ module.exports = app => {
 
     function devour(customer) {
       console.log("devouring");
-
-      // mark burger as devoured and record the id of the customer who ate it
       db.Burger
         .update(
           {
